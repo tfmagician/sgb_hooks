19a555a3e8d2913986df1e3b1074c6564fab6dfa
tfmagician <tfmagician@gmail.com>
2010-12-10 11:27:38 +0900

    Modified install script.

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-git clone https://github.com/tfmagician/sgb_hooks.git .git/hooks/
+git clone https://github.com/tfmagician/sgb_hooks.git .git/hooks/sgb_hooks
+ln -s .git/hooks/sgb_hooks/common
+ln -s .git/hooks/sgb_hooks/pre-commit
